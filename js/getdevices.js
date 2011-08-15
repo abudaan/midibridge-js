69f5e1243e3f829bbb13fcffa418bbdd7854e68b
abudaan <abudaan@gmail.com>
2011-08-16 00:34:59 +0200

    README improved, code snippets added, bug in applet fixed

---
+++ js/getdevices.js
@@ -0,0 +1,37 @@
+/**
+ * Note for IE8 users: if you include MidiBridge.js in your html, the method addEventListener will be added to the window object.
+ * In fact this method is just a wrapper around the attachEvent method.
+ */
+window.addEventListener('load', function() {
+    
+    var contentDiv = document.getElementById("content");
+    
+    midiBridge.init({ connectAllInputsToFirstOutput: false,
+        ready: function(msg){
+            contentDiv.innerHTML += msg + "<br/>";
+
+            var devices = midiBridge.getDevices();
+            for(var i = 0, max = devices.length; i < max; i++) {
+            
+                var device  = devices[i];
+                var id      = device.id;
+                var type    = device.type;
+                var name    = device.name;
+                var descr   = device.descr;
+                var available = device.available;
+                console.log(id,type,name,descr,available);
+//                console.log(device);
+            }
+            
+            midiBridge.addConnection(5,7,[midiBridge.PITCH_BEND]);
+        },
+        error: function(msg) {
+            contentDiv.innerHTML += msg + "<br/>";
+        },
+        data: function(midiEvent) {
+            contentDiv.innerHTML += midiEvent + "<br/>";
+        }        
+    });
+    
+   
+}, false);
---
+++ js/pathpanel.js
@@ -0,0 +1,82 @@
+/**
+ * Note for IE8 users: if you include MidiBridge.js in your html, the method addEventListener will be added to the window object.
+ * In fact this method is just a wrapper around the attachEvent method.
+ */
+window.addEventListener('load', function() {
+
+    var contentDiv = document.getElementById("content");
+    var currentMidiInputId = -1;
+    var currentMidiOutputId = -1;
+
+    //create a dropdown box for the midi inputs
+    var midiInputs = document.createElement("select");
+    midiInputs.setAttribute("id", "midi-in");
+    contentDiv.appendChild(midiInputs);
+    midiInputs.addEventListener("change", function(e) {
+        var device = midiInputs.options[midiInputs.selectedIndex];
+        currentMidiInputId = device.id;
+        //console.log(currentMidiInputId, currentMidiOutputId);
+        var result = midiBridge.addConnection(currentMidiInputId, currentMidiOutputId, [midiBridge.PITCH_BEND]);
+        parseResult(result);
+    }, false);
+
+    //create a dropdown box for the midi outputs
+    var midiOutputs = document.createElement("select");
+    midiOutputs.setAttribute("id", "midi-out");
+    contentDiv.appendChild(midiOutputs);
+    midiOutputs.addEventListener("change", function(e) {
+        var device = midiOutputs.options[midiOutputs.selectedIndex];
+        currentMidiOutputId = device.id;
+        //console.log(currentMidiInputId, currentMidiOutputId);
+        var result = midiBridge.addConnection(currentMidiInputId, currentMidiOutputId, [midiBridge.PITCH_BEND]);
+        parseResult(result);
+    }, false);
+
+    var devices;
+
+    midiBridge.init({
+        connectAllInputsToFirstOutput : false,
+
+        ready : function(msg) {
+            devices = midiBridge.getDevices();
+            populateDropDownMenus()
+        },
+
+        error : function(msg) {
+            contentDiv.innerHTML += msg + "<br/>";
+        },
+
+        data : function(midiEvent) {
+            contentDiv.innerHTML += midiEvent + "<br/>";
+        }
+
+    });
+
+    var populateDropDownMenus = function() {
+
+        midiInputs.appendChild(createOption("-1", "choose a midi input"));
+        midiOutputs.appendChild(createOption("-1", "choose a midi output"));
+
+        for(var deviceId in devices) {
+            var device = devices[deviceId];
+            if(device.type === "input" && device.available === "true") {
+                midiInputs.appendChild(createOption(device.id, device.name))
+            } else if(device.type === "output" && device.available === "true") {
+                midiOutputs.appendChild(createOption(device.id, device.name))
+            }
+        }
+    }
+
+    var createOption = function(id, label) {
+        var option = document.createElement("option");
+        option.setAttribute("id", id);
+        option.innerHTML = label;
+        return option;
+    }
+    
+    var parseResult = function(jsonString){
+        var data = JSON.parse(jsonString);
+        console.log(data.msg,currentMidiInputId,currentMidiOutputId);
+    }
+
+}, false);
